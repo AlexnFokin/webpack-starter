74d696b2edb8a1058c1a5b848c64bd9672e52340
a-fokin <a.fokin@sxope.com>
2023-01-10 20:30:28 +0300

    added webpack dev server

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,27 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export function buildLoaders(): webpack.RuleSetRule[] {
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ]
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader
+    ]
+}
